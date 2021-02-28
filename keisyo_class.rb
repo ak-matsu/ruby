6be7efe1a343d6cf7d14c86b9b293645ab435894
ak-matsu <sandbox.matsu@gmail.com>
2021-02-28 21:18:31 +0900

    Update keisyo_class.rb

--- keisyo_class.rb
+++ keisyo_class.rb
@@ -1,3 +1,4 @@
+#親クラス
 class Car
   def initialize(car_type, capacity)
     @name = car_type
@@ -10,7 +11,19 @@ class Car
 end
   
 #新しいクラス < 元となるクラス
+#子クラス
 class PoliceCar <Car
+
+  #オーバーライドとは、親クラスのメソッドを子クラスに
+  #同名のメソッドを定義することによって上書きすること
+  def info
+    puts "車種：#{@name} 乗車定員：#{@capacity}人 パトロール時間：２４時間"
+  end
+
+  def siren
+    puts "wow,wow"
+  end
+
 end
 
 Police_car = PoliceCar.new("パトカー",5)

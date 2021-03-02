b98aacf69291977cdc0a245b22d32f5e464e124f
ak-matsu <sandbox.matsu@gmail.com>
2021-03-03 00:18:28 +0900

    問題２終わり

--- hash2.rb
+++ hash2.rb
@@ -18,3 +18,30 @@ hash = {:one=>1,:two=>2,:three=>3}
 
 puts hash.keys
 puts hash.values
+
+
+# 問題2
+# 配列の内部に、複数のユーザーの情報をハッシュとして持つ変数user_dataがあります。
+
+# user_data = [
+#  {user: {profile: {name: 'George'}}},
+#  {user: {profile: {name: 'Alice'}}},
+#  {user: {profile: {name: 'Taro'}}},
+# ]
+# user_dataを利用して、全てのユーザーの名前だけが出力されるようにRubyでコーディングしてください。
+# ただし、出力結果は次のようになるものとします。
+
+# George
+# Alice
+# Taro
+
+
+user_data = [
+  {user: {profile: {name: 'George'}}},
+  {user: {profile: {name: 'Alice'}}},
+  {user: {profile: {name: 'Taro'}}},
+ ]
+
+ user_data.each do |date|
+  puts date[:user][:profile][:name]
+ end

cc38b891f14af9c3a9e8bca86e6f0a0e37f1a8d4
ak-matsu <sandbox.matsu@gmail.com>
2021-03-06 19:53:28 +0900

    Update DRY.rb

--- DRY.rb
+++ DRY.rb
@@ -1,25 +1,19 @@
-def select_birthplace
-  prefecture = ['北海道', '青森','沖縄']
+prefecture = ['北海道', '青森','沖縄']
+
+def select_prefecture(prefecture,category)
   num = gets.to_i
-  birthplace = prefecture[num]
-  puts birthplace
+  if category ==  'birthplace'
+    birthplace = prefecture[num]
+    puts birthplace
+  elsif  category == 'address'
+    address = prefecture[num]
+    puts address
+  elsif  category  ==  'work_address'
+    work_address =  prefecture[num]
+    puts work_address
+  end
 end
 
-def select_address
-  prefecture = ['北海道', '青森','沖縄']
-  num = gets.to_i
-  address = prefecture[num]
-  puts address
-end
-
-def select_work_address
-  prefecture = ['北海道', '青森','沖縄']
-  num = gets.to_i
-  work_address = prefecture[num]
-  puts work_address
-end
-
-
-select_birthplace
-select_address
-select_work_address
+select_prefecture(prefecture,'birthplace')
+select_prefecture(prefecture,'address')
+select_prefecture(prefecture,'work_address')

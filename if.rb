30187b95515df4857f9af97cc9691adfb0100222
ak-matsu <sandbox.matsu@gmail.com>
2021-03-07 17:55:25 +0900

    if文の修正

--- if.rb
+++ if.rb
@@ -1,9 +1,19 @@
-value = gets.to_i
+num = gets.to_i
 
-if value > 0
+if num > 0
   puts "値は正です"
-elsif value < 0
+elsif num < 0
   puts "値は負です"
 else
   puts "値は0です"
+end
+
+if num % 15 == 0
+  puts num.to_s + "は３と５の倍数です。"
+elsif num % 3 == 0
+  puts num.to_s + "は３の倍数です。"
+elsif num % 5 == 0
+  puts num.to_s + "は５の倍数です。"
+else
+  puts num.to_s + "は３の倍数でも５の倍数でもありません。"
 end

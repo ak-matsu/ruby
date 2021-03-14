6aa23ad2554f88b2328e787d5e4e60ec4d3bcf12
ak-matsu <sandbox.matsu@gmail.com>
2021-03-15 06:32:36 +0900

    効率の良い記述

--- date.rb
+++ date.rb
@@ -7,22 +7,12 @@
 # 「今日は金曜日だ ！！！」
 
 require "date"
-
 day = Date.today.wday
 
-case day
-when 1
-  puts "月曜日"
-when 2
-  puts "火曜日"
-when 3
-  puts "水曜日"
-when 4
-  puts "木曜日"
-when 5
-  puts "金曜日 !!"
-when 6
-  puts "土曜日"
-when 7
-  puts "日曜日"
+days = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
+
+if day == 5
+  puts "今日は#{days[day]}だ！！！"
+else
+  puts "今日は#{days[day]}"
 end

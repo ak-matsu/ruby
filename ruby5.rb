86b399d0e119ca1d362fb438a03ecaec6fa265e5
ak-matsu <sandbox.matsu@gmail.com>
2021-03-15 06:09:56 +0900

    エラー原因特定と修正

--- ruby5.rb
+++ ruby5.rb
@@ -1,10 +1,16 @@
 # エラー原因を解決せよ
 
+# 原因
+# メソッドの外側で定義した変数は、メソッドの内側では使えない。
+# そのため、1行目のpriceはcalculate_price_with_taxメソッドの中では
+# 使えずにエラーが起きてしまった。
+
+# 実引数と仮引数を使う必要がある。
 price = 300
 
-def calculate_price_with_tax
+def calculate_price_with_tax(price)
   tax = 0.1
-  return price + price * tax
+  puts price + price * tax
 end
 
-calculate_price_with_tax
+calculate_price_with_tax(price)

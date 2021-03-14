d616f2bce0892d2f0fce325c7f4361435b11b214
ak-matsu <sandbox.matsu@gmail.com>
2021-03-15 06:19:00 +0900

    6問目

---
+++ ruby6.rb
@@ -0,0 +1,10 @@
+# "晴れ" という文字列を引数で渡した時に
+# 明日の天気は晴れです
+# とターミナルに表示されるようなメソッドを作成してください。
+# 呼び出し方：get_weather_forecast(“晴れ”)
+
+def get_weather_forecast(tenki)
+  puts ("明日の天気は#{tenki}です。")
+end
+
+get_weather_forecast("晴れ")

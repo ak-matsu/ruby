fdaec09d43834beb07bc937a03fed655da785082
ak-matsu <sandbox.matsu@gmail.com>
2021-03-24 21:55:54 +0900

    search完成

--- search.rb
+++ search.rb
@@ -2,30 +2,18 @@
 # 以下の配列から、数を探して何番目に含まれているか結果を返すメソッドをsearchとeach_with_indexを用いて作成してください。
 # input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
 
+def search(taraget_num,input)
 
-
-# input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-# def search
-#   puts ("#{input}番目にあります。")
-# end
-# num = gets.to_i
-# search(num,input)
+  input.each_with_index do |num, index|
+      if num == taraget_num
+        puts "#{index +1}番目にあります。"
+        return
+      end
+    end
+    puts "その数は含まれていません。"
+end
 
 input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
-# fruits = ["メロン", "バナナ", "アップル"]
-
+puts "数字を入力してください"
 num = gets.to_i
-
-# def search
-  # fruits.each_with_index do |item, i|
-  input.each_with_index do |item, i|
-      #  puts "#{i}番目のフルーツは、#{item}です。"
-      if num == item
-        puts "#{i}番目にあります。"
-      else
-        puts "ありません。"
-      end
-  end
-
-
-# search(num,input)
+search(num,input)

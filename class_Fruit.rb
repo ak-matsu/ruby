48a019110f9fd2298e9e6c9b29cc3da70ccdfce7
ak-matsu <sandbox.matsu@gmail.com>
2021-04-02 22:12:19 +0900

    回答

--- class_Fruit.rb
+++ class_Fruit.rb
@@ -1,21 +1,30 @@
-# 雛形
-
 class Fruit
-  def クラスメソッド
-    # 正しくメソッドを定義した上で、ここに処理を記入してください
+
+  # クラスメソッド
+  def self.fresh
+    puts "採れたて新鮮な果実です"
   end
  
-  def initialize
-    # ここに処理を記入してください
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
- 
-  def インスタンスメソッド
-    # 正しくメソッドを定義した上で、ここに処理を記入してください
+
+  # インスタンスメソッド
+  def introduce
+    puts "#{@name}は#{@price}円です"
   end
- end
- 
- 
- # 3つのインスタンスを生成してください
- 
- # クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示してください
- # インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示してください
+end
+
+# 3つのインスタンスを生成
+# class Fruitをnewメソッドでインスタンス化し値を入れ、それぞれの変数へ代入
+apple = Fruit.new("リンゴ", 120)
+orange = Fruit.new("オレンジ", 200)
+strawberry = Fruit.new("イチゴ", 60)
+
+# クラスメソッドを実行
+Fruit.fresh
+# 各インスタンス変数を用いてインスタンスメソッドを実行。
+apple.introduce
+orange.introduce
+strawberry.introduce

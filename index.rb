35d98ae96f17f59bd7c1029d90a065375f20f694
ak-matsu <sandbox.matsu@gmail.com>
2021-04-06 20:20:23 +0900

    Create index.rb

---
+++ index.rb
@@ -0,0 +1,7 @@
+# 任意の文字列に"code"が、左から何文字目に出てくるかを返し、その数を出力するメソッドを作りましょう。
+
+# count_code("codexxcode") → 1
+# count_code("aaacodebbb") → 4
+# count_code("cozexxcode") → 7
+
+str.index(検索したい文字列, [検索を開始する位置])

b9e3ee10794b3f6f52bbc019776b858a01997c17
ak-matsu <sandbox.matsu@gmail.com>
2021-04-07 20:12:41 +0900

    Create if_else.rb

---
+++ if_else.rb
@@ -0,0 +1,28 @@
+# 問題.1
+# 20時から翌朝7時までにオウムに喋られると問題があるのでその場合は「NG」、
+# それ以外は「OK」と出力するメソッドを作成します。
+# オウムが喋る時をtrue、喋らない時をfalseと入力することにし、時刻も同時に入力します。
+
+# 呼び出し方：
+# parrot_trouble(talking, hour)
+
+# 出力例：
+# parrot_trouble(true, 6) → NG
+# parrot_trouble(true, 7) → OK
+# parrot_trouble(false, 6) → OK
+# parrot_trouble(false, 7) → OK
+
+
+def parrot_trouble(talking,hour)
+  if hour < 7 && hour <20
+    puts "OK"
+  else
+    puts "NG"
+  end
+end
+
+
+# parrot_trouble(true, 6)
+# parrot_trouble(true, 7) → OK
+# parrot_trouble(false, 6) → OK
+# parrot_trouble(false, 7) → OK

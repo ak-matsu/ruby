bf3a0e7e7941ab9c43414f15e48857f1729c7ead
ak-matsu <sandbox.matsu@gmail.com>
2021-04-11 19:30:52 +0900

    Update calc.rb

--- calc.rb
+++ calc.rb
@@ -11,29 +11,36 @@
 
 # > 足し算結果と掛け算結果の合計値は11です
 # 以下の雛形を参考にしましょう。
-# def addition(a, b)
-#   # 10の位と1の位の数字に対して、足し算を行う
 
-# end
+def addition(a, b)
+  # 10の位と1の位の数字に対して、足し算を行う
+  a + b
 
-# def multiplication(a,b)
-#   # 10の位と1の位の数字に対して、掛け算を行う
+end
 
-# end
+def multiplication(a,b)
+  # 10の位と1の位の数字に対して、掛け算を行う
+  a * b
 
-# def slice_num(num)
-#   # 10の位の計算
+end
 
-#   # 1の位の計算
+def slice_num(num)
+  # 10の位の計算
+  tens_place = (num / 10) % 10
 
-# end
+  # 1の位の計算
+  ones_place = num % 10
+  return tens_place, ones_place
+end
 
-# puts "二桁の整数を入力してください"
-# input = gets.to_i
+puts "二桁の整数を入力してください"
+input = gets.to_i
 
-# X, Y = slice_num(input)
-# #  additionメソッドにX,Yを引数として渡し、処理結果を変数add_resultに代入する。
+X, Y = slice_num(input)
+#  additionメソッドにX,Yを引数として渡し、処理結果を変数add_resultに代入する。
+add_result = addition(X,Y)
 
-# #  multiplicationメソッドにX,Yを引数として渡し、処理結果を変数multiple_resultに代入する。
+#  multiplicationメソッドにX,Yを引数として渡し、処理結果を変数multiple_resultに代入する。
+multiple_result = multiplication(X,Y)
 
-# puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
+puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"

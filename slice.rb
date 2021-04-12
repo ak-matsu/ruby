76b227e04d5fb528aa2464f142ca2c32f8a3bc55
ak-matsu <sandbox.matsu@gmail.com>
2021-04-12 16:03:27 +0900

    Update slice.rb

--- slice.rb
+++ slice.rb
@@ -1,9 +1,9 @@
-# def missing_char(str,n)
-#   str.slice!(n -1)
-#   puts str
-# end
+def missing_char(str,n)
+  str.slice!(n -1)
+  puts str
+end
 
-# missing_char('kitten',1)
+missing_char('kitten',1)
 
 
 # 問題.1
@@ -15,19 +15,31 @@
 # extra_end('ab') → 'ababab'
 # extra_end('Hi') → 'HiHiHi'
 
-# # 配列を作成します
-# array = [0,1,2,3,4,5,6]
+# 配列を作成します
+array = [0,1,2,3,4,5,6]
 
-# # 配列から引数で指定した要素をsliceします
-# ele1 = array.slice(1)
-# puts ele1
-# #=> 1
+# 配列から引数で指定した要素をsliceします
+ele1 = array.slice(1)
 
-# # 配列番号1から4つ分の要素をsliceします
-# ele2 = array.slice(1,4)
-# puts ele2
-# #=> 1 2 3 4
+puts ele1
+#=> 1
 
-# # 配列はもとのままです
-# puts array 
-# #=> [0,1,2,3,4,5,6]
+# 配列番号1から4つ分の要素をsliceします
+ele2 = array.slice(1,4)
+puts ele2
+#=> 1 2 3 4
+
+# 配列はもとのままです
+puts array 
+#=> [0,1,2,3,4,5,6]
+
+def extra_end(str)
+  right2 = str.slice(- 2, 2)
+  puts right2 * 3
+end
+
+
+# 出力例：
+extra_end('Hello')
+extra_end('ab')
+extra_end('Hi')

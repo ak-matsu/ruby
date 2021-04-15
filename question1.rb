7e0cc8c5ace1f67876a4b83bf3c83adb2637253a
ak-matsu <sandbox.matsu@gmail.com>
2021-04-15 20:59:19 +0900

    Update question1.rb

--- question1.rb
+++ question1.rb
@@ -29,6 +29,28 @@ end
 
 fizz_buzz
 
+def fizz_buzz
+  num = 1
+  while (num <= 100) do
+    if (num % 3 == 0) && (num % 5 == 0)
+      puts "FizzBuzz"
+    elsif (num % 3) == 0
+      puts "Fizz"
+    elsif (num % 5) == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+
+    num = num + 1
+  end
+end
+
+fizz_buzz
+
+
+
+
 # ヒント
 # ① 以下の4つに条件を分岐しましょう
 

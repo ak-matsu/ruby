233f581e6cc0989eefe8e31beae1b8a49235e00a
ak-matsu <sandbox.matsu@gmail.com>
2021-05-16 21:45:27 +0900

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,13 +1,12 @@
 class Book
-  def initialize(name)
-    @name = name    
-  end
+  attr_reader :title, :price
 
-  def name
-    @name
+  def initialize(title, price)
+   @title = title
+   @price = price
   end
-
 end
 
-dog = Dog.new("ポチ")
-puts dog.name
+book = Book.new("プロを目指す人のためのruby入門",3218)
+puts book.title
+puts "#{book.price}円"

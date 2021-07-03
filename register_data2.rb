8be0914014d8f5c0063c085506adb5ce653dac39
ak-matsu <sandbox.matsu@gmail.com>
2021-07-03 21:15:07 +0900

    Update register_data2.rb

--- register_data2.rb
+++ register_data2.rb
@@ -19,19 +19,54 @@
 
 # 無効な値です
 
+# 2
+# ・def register_dataメソッドで情報を入力させ、ハッシュに格納したあと、配列に格納するコードを書きましょう。
+# 　登録する情報は、名前,パンチ力,キック力,ジャンプ力です。
+# 　力は1~100までの数値で入力させます。
+# 　1~100以外の値が入力された場合は”100以下の数字を入力してください”と表示し、入力フェーズに戻るようにします。
+# ・show_data_listメソッドで、登録した情報から名前だけを取り出して一覧表示させましょう。
 
 def register_data
 
   puts '名前を入力してください'
+  name = gets.chomp
+
   puts 'パンチ力を入力してください(0 ~ 100)'
+  panch = power(gets.to_i)
+
   puts 'キック力を入力してください(0 ~ 100)'
+  kick = power(gets.to_i)
+
   puts 'ジャンプ力を入力してください(0 ~ 100)'
+  jump = power(gets.to_i)
+
+  person = {name: name, punch: punch, kick: kick, jump: jump}
+  data << person
 
 end
 
-def show_data_list
+def power(power)
+  while power > 100
+    puts '100以下の数字を入力してください。'
+      power = gets.to_i
+  end
+  return power
+end
+
+def show_data_list(data)
   puts '見たい人の番号を選択してください'
+  data.each_with_index do |person,  index|
+    puts "#{index + 1}: #{person[:name]}"
+  end
+  input = gets.to_i - 1
+  person = data[input]
+  puts "名前: #{person[:name]}"
+  puts "パンチ力: #{person[:punch]}"
+  puts "キック力: #{person[:kick]}"
+  puts "ジャンプ力: #{person[:jump]}"
 end
+
+data = []
 
 while true
   puts '選択してください'

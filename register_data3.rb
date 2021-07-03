4ed629fabcb3be25787af2dd9c2b39c58bb5e00d
ak-matsu <sandbox.matsu@gmail.com>
2021-07-03 22:56:25 +0900

    Update register_data3.rb

--- register_data3.rb
+++ register_data3.rb
@@ -26,50 +26,52 @@
 # each_with_index
 # 使用すると、配列のインデックス番号(配列の要素番号)が取得できる
 
-def register_data(data)
-
-# 1.車種、1Lあたりの走行可能距離(km/l)、乗車人数の入力をユーザーに入力を行って貰う
-# 2.上記の値の保存を行う
-  puts "車種を入力してください"
-  syasyu = gets.chomp
-  puts "1Lあたりの走行可能距離(km/l)を入力してください"
-  distance = gets.to_f
-  puts "乗車人を入力してください"
-  person = gets.to_i
-
-  car = {syasyu: syasyu, distance: distance, person: person}
-  data << car
-
+def register_data(cars)
+  puts "車種の入力をして下さい。"
+  type = gets.chomp
+  puts "1Lあたりの走行可能距離(km/l)を入力して下さい。"
+  fuel_economy = gets.to_f
+  puts "乗車可能人数を入力して下さい。"
+  capacity = gets.to_i
+  car = { type: type, fuel_economy: fuel_economy, capacity: capacity }
+  cars << car
 end
 
-def show_data_list(data)
-  puts "見たい人の番号を選択してください"
-  data.each_with_index do |car, index|
-    puts "#{index + 1}: #{car[:syasyu]}"
+def show_cars(cars)
+  cars.each_with_index do |car, index|
+    puts "[#{index}]: #{car[:type]}"
   end
-  input = gets.to_i - 1
-  show_data(data[input])
-end
-
-def show_data(data)
-  puts "車種:#{data[:syasyu]}"
-  puts "1Lあたりの走行可能距離:#{data[:distance]}"
-  puts "乗車人数:#{data[:person]}"
-end
-
-data = []
-while true
-  puts "選択してください"
-  puts "[0]登録する"
-  puts "[1]データを確認する"
-  puts "[2]終了する"
+  puts "確認したい番号を入力して下さい。"
   input = gets.to_i
+  car = cars[input]
+  if car
+    show_data(car)
+  else
+    puts "該当する番号はありません。"
+  end
+end
 
-  if input == 0
-    register_data(data)
-  elsif input == 1
-    show_data_list(data)
-  elsif input == 2
+def show_data(car)
+  puts car[:type]
+  puts "1Lあたりの走行可能距離:#{car[:fuel_economy]}km/l"
+  puts "乗車人数:#{car[:capacity]}人"
+end
+
+cars = []
+
+while true do
+  puts "番号を入力して下さい"
+  puts "[0]:登録をする"
+  puts "[1]:データを確認する"
+  puts "[2]:終了する"
+  input = gets.chomp
+
+  case input
+  when "0"
+    register_data(cars)
+  when "1"
+    show_cars(cars)
+  when "2"
     exit
   else
     puts "無効な値です"

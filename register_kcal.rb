0df877c1142cd4f09eada23860671f64f3cfb45d
ak-matsu <sandbox.matsu@gmail.com>
2021-07-04 18:15:34 +0900

    Create register_kcal.rb

---
+++ register_kcal.rb
@@ -0,0 +1,16 @@
+class Food
+  # ここにクラスの定義を書き加えてください
+end
+
+while true do
+  puts "[0]:カロリーを入力する"
+  puts "[1]:カロリーの合計を見る"
+  input = gets.to_i
+
+  if input == 0
+    Food.input           # カロリーの入力
+  elsif input == 1
+    Food.show_all_calory # カロリーの合計を表示
+    exit
+  end
+end

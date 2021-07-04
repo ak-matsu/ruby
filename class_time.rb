7897d2fa714dac344b30f8f6b7f4db42f3378a5c
ak-matsu <sandbox.matsu@gmail.com>
2021-07-04 17:42:02 +0900

    Update class_time.rb

--- class_time.rb
+++ class_time.rb
@@ -12,5 +12,10 @@
 # 以下のように利用します。
 
 # # 現在時刻
-# now = Time.new
-# puts now
+now = Time.new
+puts now
+
+now = Time.new
+puts "現在は西暦#{now.year}年#{now.month}月#{now.day}日"
+week = ["日","月","火","水","木","金","土"]
+puts week[now.wday] + '曜日です' 

ca142cd4b4049e3af0811a4a847b58f0319105bd
ak-matsu <sandbox.matsu@gmail.com>
2021-07-04 18:16:22 +0900

    Update register_kcal.rb

--- register_kcal.rb
+++ register_kcal.rb
@@ -1,5 +1,42 @@
 class Food
-  # ここにクラスの定義を書き加えてください
+  @@foods = []
+
+  def self.input
+    puts "料理名を入力してください:"
+    name = gets.chomp
+    puts "カロリーを入力してください:"
+    calory = gets.to_i
+
+    food = Food.new(name, calory)
+    @@foods << food
+  end
+
+  def self.show_all_calory
+    all_calory = 0
+    line = "-----------------------------"
+    puts line
+
+    @@foods.each do |food|
+      puts "#{food.name}  :#{food.calory}kcal"
+      all_calory = all_calory + food.calory
+    end
+
+    puts line
+    puts "カロリー合計  :#{all_calory}kcal"
+  end
+
+  def initialize(name, calory)
+    @name = name
+    @calory = calory
+  end
+
+  def name
+    return @name
+  end
+
+  def calory
+    return @calory
+  end
 end
 
 while true do

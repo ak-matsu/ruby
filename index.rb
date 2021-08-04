04bfa213af53aaa2b805c8339fb286a31f728174
ak-matsu <sandbox.matsu@gmail.com>
2021-08-04 22:24:02 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -14,3 +14,15 @@ end
 count_code("codexxcode")
 count_code("aaacodebbb")
 count_code("cozexxcode")
+
+
+text = "プログラミングを学ぼう"
+
+# 文字列「Progateで」と変数textを連結して、出力してください
+puts "Progateで" + text
+
+length = 8
+width = 9
+
+# 変数lengthと変数widthを掛けて出力してください
+puts length * width

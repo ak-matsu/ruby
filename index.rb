2942e6ea1c9a9e034de6cc69f22e2f26115d1e0c
ak-matsu <sandbox.matsu@gmail.com>
2021-08-05 21:43:52 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -26,3 +26,11 @@ width = 9
 
 # 変数lengthと変数widthを掛けて出力してください
 puts length * width
+
+languages = ["日本語", "英語", "スペイン語"]
+
+# インデックス番号が1の要素を出力してください
+puts languages[1]
+
+# インデックス番号が0の要素を使って「◯◯を話せます」となるように出力してください
+puts languages[0] + "を話せます"

d646d9d58170edcf7ee95cf9c86006fd66069e49
ak-matsu <sandbox.matsu@gmail.com>
2021-08-07 23:06:03 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -43,3 +43,12 @@ end
 
 puts "-----自己紹介-----"
 introduce
+
+# print_infoメソッドを定義してください
+def print_info
+  puts "わんこでんきへようこそ！"
+  puts "今日はヘッドホンがセール中です！"
+end
+
+# print_infoメソッドを呼び出してください
+print_info

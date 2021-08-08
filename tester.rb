3bc73a34c4eca6e293cd270319458ec9767e6982
ak-matsu <sandbox.matsu@gmail.com>
2021-08-08 23:51:40 +0900

    methodadd

--- tester.rb
+++ tester.rb
@@ -1,7 +1,9 @@
-def palindrome_tester(s)
+def palindrome_tester(s,t)
   if s == s.reverse
     puts "It's a palindrome!"
-  else
+  elsif t == t.reverse
     puts "It's not a palindrome."
   end
 end
+
+puts palindrome_tester("racecar","onomatopoeia")

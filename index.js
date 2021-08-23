24e349f8e828aaa69faf6b289ddc9a467a8dfa03
ak-matsu <sandbox.matsu@gmail.com>
2021-08-23 18:29:36 +0000

    オリジナル時刻表示

---
+++ index.js
@@ -0,0 +1,54 @@
+function set2(num) {
+  // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+  let ret;
+  if (num < 10) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
+}
+function showClock() {
+  const nowTime = new Date(); //現在日時
+  const nowHour = set2(nowTime.getHours()); //時間
+  const nowMin = set2(nowTime.getMinutes()); //分
+  const nowSec = set2(nowTime.getSeconds()); //秒
+  const msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("showTime").innerHTML = msg;
+}
+setInterval('showClock()', 1000);
+
+
+//今日の日付データを変数に格納
+//変数は"today"とする
+var today=new Date(); 
+
+//年・月・日・曜日を取得
+var year = today.getFullYear();
+var month = today.getMonth()+1;
+var week = today.getDay();
+var day = today.getDate();
+
+var week_ja= new Array("日","月","火","水","木","金","土");
+
+//年・月・日・曜日を書き出す
+document.write("DC"+(year-(1998))+"年"+month+"月"+day+"日 "+week_ja[week]+"曜日");
+
+
+//時刻データを取得して変数に格納する
+//変数は"time"とする
+var time= new Date();
+
+//時・分・秒を取得
+var hour = time.getHours();
+var minute = time.getMinutes();
+var second = time.getSeconds();
+
+document.write(hour+"時",+minute+"分"+second+"秒");
+
+function showClock1() {
+  var nowTime = new Date();
+  var nowHour = nowTime.getHours();
+  var nowMin  = nowTime.getMinutes();
+  var nowSec  = nowTime.getSeconds();
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea").innerHTML = msg;
+}
+setInterval('showClock1()',1000);
